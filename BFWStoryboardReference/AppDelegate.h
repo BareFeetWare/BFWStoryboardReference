5e1ce17d96fc822a18abb39e12e79ef19e040cb2
Tom Brodhurst-Hill <developer@barefeetware.com>
2015-12-10 14:17:25 +1100

    Initial Commit

---
+++ BFWStoryboardReference/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFWStoryboardReference
+//
+//  Created by Tom Brodhurst-Hill on 10/12/2015.
+//  Copyright © 2015 BareFeetWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
